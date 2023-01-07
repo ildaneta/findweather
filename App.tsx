c03ec0149e601241437ddc9cc3f56d451c020127
ildaneta <iildaemanoely@gmail.com>
2023-01-07 18:14:05 +0100

    refactor: change from .js to .tsx file extension

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import {
+  useFonts,
+  Overpass_300Light,
+  Overpass_400Regular,
+  Overpass_600SemiBold,
+  Overpass_700Bold,
+} from "@expo-google-fonts/overpass";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Overpass_300Light,
+    Overpass_400Regular,
+    Overpass_600SemiBold,
+    Overpass_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontFamily: "Overpass_300Light", fontSize: 20 }}>
+        Find Weather!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
