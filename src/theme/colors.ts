8b0470aea8dca717c5fcf2d0a70cd0d1afd9626e
ildaneta <iildaemanoely@gmail.com>
2023-01-08 13:22:09 +0100

    feat: create colors file

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: "#fff",
+  gray100: "#afafaf",
+  gray200: "#a1a1a1",
+  gray300: "#838384",
+  gray400: "#7E7E7E",
+  gray500: "#6F7072",
+  gray600: "#4D4D4D",
+  dark100: "#33383E",
+  dark200: "#2C3137",
+  dark300: "#23262B",
+  dark400: "#212428",
+  dark: "#1B1D22",
+};
