e2ef09e48af781e6930ab01267498c62debb561a
ildaneta <iildaemanoely@gmail.com>
2023-01-08 13:22:20 +0100

    feat: create font family file

---
+++ src/theme/fontFamily.ts
@@ -0,0 +1,6 @@
+export const fontFamily = {
+  OverpassLight: "Overpass_300Light",
+  OverpassRegular: "Overpass_400Regular",
+  OverpassSemiBold: "Overpass_600SemiBold",
+  OverpassBold: "Overpass_700Bold"
+}
