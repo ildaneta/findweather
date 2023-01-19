2498cef593fe11894eef84f6bf97280ad108ac23
ildaneta <iildaemanoely@gmail.com>
2023-01-19 21:41:19 +0100

    refactor: add route type and navigate to search screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -18,6 +18,14 @@ import DropMiniaturePNG from "../../assets/drop-miniature.png";
 import WindMiniaturePNG from "../../assets/wind-miniature.png";
 import RainingCloudPNG from "../../assets/raining-cloud-miniature.png";
 import ClimateChangePNG from "../../assets/climate-change.png";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { IStackRoutes } from "../../routes/stack.routes";
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<IStackRoutes, "Home">;
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
 
 const dataWeatherDescription = [
   {
@@ -72,7 +80,7 @@ const dataCardHourTemperature = [
   },
 ];
 
-const EmptyStateContent = () => {
+const EmptyStateContent = ({ navigation }: Props) => {
   return (
     <Styled.Container>
       <Styled.ContainerEmptyState>
@@ -99,7 +107,10 @@ const EmptyStateContent = () => {
 
         <Divider top={100} />
 
-        <TouchableOpacity onPress={() => {}} activeOpacity={0.75}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Search")}
+          activeOpacity={0.75}
+        >
           <Text
             fontFamily={theme.fontFamily.OverpassRegular}
             fontSize={theme.fontSize.md22}
@@ -216,10 +227,10 @@ const FullContent = () => (
   </>
 );
 
-const Home = (): JSX.Element => {
+const Home = ({ navigation }: Props): JSX.Element => {
   return (
     <Styled.ScrollView>
-      <EmptyStateContent />
+      <EmptyStateContent navigation={navigation} />
     </Styled.ScrollView>
   );
 };
