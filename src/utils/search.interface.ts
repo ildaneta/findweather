c0d42139c01cebceca8088a145088f441f8e89d3
ildaneta <iildaemanoely@gmail.com>
2023-01-21 23:24:46 +0100

    refactor: separate search interface in new interfaces to be reused

--- src/utils/search.interface.ts
+++ src/utils/search.interface.ts
@@ -1,45 +1,12 @@
 export interface ISearchData {
-  location: {
-    name: string,
-    region: string,
-    country: string,
-    lat: number,
-    lon: number,
-    tz_id: string,
-    localtime_epoch: number,
-    localtime: string
-  },
-  current: {
-    last_updated_epoch: number,
-    last_updated: string,
-    temp_c: number,
-    temp_f: number,
-    is_day: number,
-    condition: ICondition,
-    wind_mph: number,
-    wind_kph: number,
-    wind_degree: number,
-    wind_dir: string,
-    pressure_mb: number,
-    pressure_in: number,
-    precip_mm: number,
-    precip_in: number,
-    humidity: number,
-    cloud: number,
-    feelslike_c: number,
-    feelslike_f: number,
-    vis_km: number,
-    vis_miles: number,
-    uv: number,
-    gust_mph: number,
-    gust_kph: number,
-  },
+  location: ILocation,
+  current: ICurrent,
   forecast: {
     forecastday: Array<IForecastData>
   }
 }
 
-interface IForecastData {
+export interface IForecastData {
   date: string,
   date_epoch: number,
   day: {
@@ -119,6 +86,43 @@ export interface ICondition {
   text: string,
   icon: string,
   code: number
+}
+
+export interface ILocation {
+  name: string,
+  region: string,
+  country: string,
+  lat: number,
+  lon: number,
+  tz_id: string,
+  localtime_epoch: number,
+  localtime: string
+}
+
+export interface ICurrent {
+  last_updated_epoch: number,
+  last_updated: string,
+  temp_c: number,
+  temp_f: number,
+  is_day: number,
+  condition: ICondition,
+  wind_mph: number,
+  wind_kph: number,
+  wind_degree: number,
+  wind_dir: string,
+  pressure_mb: number,
+  pressure_in: number,
+  precip_mm: number,
+  precip_in: number,
+  humidity: number,
+  cloud: number,
+  feelslike_c: number,
+  feelslike_f: number,
+  vis_km: number,
+  vis_miles: number,
+  uv: number,
+  gust_mph: number,
+  gust_kph: number,
 }
 
 
