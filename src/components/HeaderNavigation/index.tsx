d478a270a230aae4dab3822c7cfb42d92fa33ec3
ildaneta <iildaemanoely@gmail.com>
2023-01-23 23:27:11 +0100

    refactor: insert new optional property icon and change logic

--- src/components/HeaderNavigation/index.tsx
+++ src/components/HeaderNavigation/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacityProps } from "react-native";
+import { Platform, TouchableOpacityProps, View } from "react-native";
 import Text from "../Text";
 import { AntDesign } from "@expo/vector-icons";
 import theme from "../../theme";
@@ -8,10 +8,12 @@ import Styled from "./styles";
 
 interface IHeaderNavigation extends TouchableOpacityProps {
   titlePage: string;
+  icon?: React.ReactNode;
 }
 
 const HeaderNavigation = ({
   titlePage,
+  icon,
   ...rest
 }: IHeaderNavigation): JSX.Element => {
   return (
@@ -20,14 +22,36 @@ const HeaderNavigation = ({
         <AntDesign name="left" size={20} color={theme.colors.white} />
       </Styled.BackButton>
 
-      <Text
-        fontFamily={theme.fontFamily.OverpassRegular}
-        fontSize={theme.fontSize.sm18}
-        color={theme.colors.white}
-        style={{ marginLeft: "30%" }}
-      >
-        {titlePage}
-      </Text>
+      {icon ? (
+        <View
+          style={{
+            alignItems: Platform.OS === "android" ? "center" : "stretch",
+            flexDirection: "row",
+            marginLeft: "20%",
+          }}
+        >
+          {icon}
+          <Text
+            fontFamily={theme.fontFamily.OverpassRegular}
+            fontSize={theme.fontSize.sm18}
+            color={theme.colors.white}
+            style={{ marginLeft: 10 }}
+          >
+            {titlePage}
+          </Text>
+
+          <View />
+        </View>
+      ) : (
+        <Text
+          fontFamily={theme.fontFamily.OverpassRegular}
+          fontSize={theme.fontSize.sm18}
+          color={theme.colors.white}
+          style={{ marginLeft: "30%" }}
+        >
+          {titlePage}
+        </Text>
+      )}
     </Styled.Container>
   );
 };
