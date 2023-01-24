a87f20a87f77f5c5c9eed0ce2ef90237131d72ba
ildaneta <iildaemanoely@gmail.com>
2023-01-24 21:26:27 +0100

    refactor: add the parameter lang portuguese into findweatherAPI

--- src/services/findweather-api.ts
+++ src/services/findweather-api.ts
@@ -4,6 +4,6 @@ const { API_KEY } = process.env
 
 export const FindWeatherAPI = {
   getForecast: (city: string) => {
-    return api.get(`forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=no&alerts=no`)
+    return api.get(`forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=no&alerts=no&lang=pt`)
   }
 }
