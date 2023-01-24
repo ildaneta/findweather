353b7cee043afdaa46d3f64f544a20188b87eda2
ildaneta <iildaemanoely@gmail.com>
2023-01-24 21:24:18 +0100

    refactor: remove some styles

--- src/components/Temperature/styles.ts
+++ src/components/Temperature/styles.ts
@@ -2,14 +2,10 @@ import styled from 'styled-components/native';
 
 const TemperatureContainer = styled.View`
   flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  height: 100px;
 `;
 
 const MaxTemperatureContainer = styled.View`
   flex-direction: row;
-  align-items: center;
 `;
 
 const MinTemperatureContainer = styled.View`
