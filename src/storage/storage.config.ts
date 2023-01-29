5e838b0867cbcdde29023ff54fc22421d5a238a5
ildaneta <iildaemanoely@gmail.com>
2023-01-29 19:56:47 +0100

    feat: add a new storage key COUNTRY_CODE

--- src/storage/storage.config.ts
+++ src/storage/storage.config.ts
@@ -1,5 +1,7 @@
 const USER_FIRST_TIME = '@findweather:welcome';
 
-const CITY_NAME = '@findweather:city_name'
+const CITY_NAME = '@findweather:city_name';
 
-export {USER_FIRST_TIME, CITY_NAME}
+const COUNTRY_CODE = '@findweather:country_code';
+
+export {USER_FIRST_TIME, CITY_NAME, COUNTRY_CODE}
