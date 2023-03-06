8a312b0bb8d214f3233bc1eb073cf191b588309e
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-03-06 07:19:08 -0300

    refactor: add global parameters

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `http://api.weatherapi.com/v1/`
+  baseURL: `http://api.weatherapi.com/v1/`,
+  params: {
+    key: process.env.API_KEY,
+  },
 })
 
